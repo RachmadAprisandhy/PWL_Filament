7b0f2da5398057176e3dcc9dcc6f65dd0fbf6525
RachmadAprisandhy <124744940+RachmadAprisandhy@users.noreply.github.com>
2026-03-08 21:29:34 +0700

    commit lagi

--- app/Filament/Resources/Posts/Schemas/PostForm.php
+++ app/Filament/Resources/Posts/Schemas/PostForm.php
@@ -5,20 +5,81 @@ namespace App\Filament\Resources\Posts\Schemas;
 use Filament\Schemas\Schema;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Select;
+use Filament\Forms\Components\ColorPicker;
+use Filament\Forms\Components\MarkdownEditor;
+use Filament\Forms\Components\RichEditor;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\TagsInput;
+use Filament\Forms\Components\Checkbox;
+use Filament\Forms\Components\DatePicker;
+use Filament\Tables\Columns\Column;
+use Filament\Schemas\Components\Section;
+use Filament\Support\Icons\Heroicons;
+use Filament\Schemas\Components\Group;
+
 
 class PostForm
 {
-    public static function configure(Schema $schema): Schema
-    {
-        return $schema
-            ->components([
-                //
-                TextInput::make('title'),
-                TextInput::make('slug'),
-                Select::make('category_id')
-                ->relationship('category', 'name')
-                ->preload()
-                ->searchable(),
-                ]);
-    }
+   public static function configure(Schema $schema): Schema
+{
+    return $schema
+        ->columns(3)
+        ->components([
+            
+            Section::make('Post Details')
+                ->description('Fill the details of the post')
+                ->icon('heroicon-o-document-text')
+                ->schema([
+                    Group::make([
+                        TextInput::make('title')
+                            ->required()
+                            ->minLength(5) 
+                            ->validationMessages([
+                                'min' => 'Judul terlalu pendek, minimal 5 karakter ya!',
+                            ]),
+                            
+                        TextInput::make('slug')
+                            ->required()
+                            ->minLength(3)
+                            ->unique(table: 'posts', column: 'slug') 
+                            ->validationMessages([
+                                'unique' => 'Slug ini sudah dipakai, coba yang lain.',
+                            ]),
+                            
+                        Select::make('category_id')
+                            ->relationship('category', 'name')
+                            ->required()
+                            ->preload()
+                            ->searchable(),
+                            
+                        ColorPicker::make('color'),
+                    ])->columns(2),
+                    
+                    MarkdownEditor::make('content'),
+                ])->columnSpan(2), 
+
+            Group::make()
+                ->schema([
+                    Section::make("Image Upload")
+                        ->icon('heroicon-o-photo')
+                        ->schema([
+                            FileUpload::make('image')
+                                ->image()
+                                ->required() 
+                                ->disk('public')
+                                ->directory('posts')
+                                ->visibility('public'),
+                        ]),
+                    
+                    Section::make("Meta Information")
+                        ->icon('heroicon-o-identification')
+                        ->schema([
+                            TagsInput::make('tags'),
+                            Checkbox::make('published'),
+                            DatePicker::make('published_at'),
+                        ]),
+                ])
+                ->columnSpan(1), 
+        ]);
+}
 }
--- app/Filament/Resources/Posts/Tables/PostsTable.php
+++ app/Filament/Resources/Posts/Tables/PostsTable.php
@@ -6,6 +6,10 @@ use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
 use Filament\Tables\Table;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Columns\ColorColumn;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Tables\Columns\IconColumn;
 
 class PostsTable
 {
@@ -13,7 +17,17 @@ class PostsTable
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('title'),
+                TextColumn::make('slug'),
+                TextColumn::make('category.name'),
+                ColorColumn::make('color'),
+                IconColumn::make('published')
+                ->boolean(),
+             ImageColumn::make('image')
+            ->disk('public')
+            ->visibility('public')
+            ->height(50)
+            ->square()
             ])
             ->filters([
                 //
@@ -24,6 +38,7 @@ class PostsTable
             ->toolbarActions([
                 BulkActionGroup::make([
                     DeleteBulkAction::make(),
+            
                 ]),
             ]);
     }
