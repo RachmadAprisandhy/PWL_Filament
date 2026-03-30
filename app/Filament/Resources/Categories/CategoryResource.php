e2da46281806c5afcad056782b1b865e70507bc6
RachmadAprisandhy <124744940+RachmadAprisandhy@users.noreply.github.com>
2026-03-30 22:11:20 +0700

    commit jobsheet 14 half

--- app/Filament/Resources/Categories/CategoryResource.php
+++ app/Filament/Resources/Categories/CategoryResource.php
@@ -13,6 +13,7 @@ use Filament\Resources\Resource;
 use Filament\Schemas\Schema;
 use Filament\Support\Icons\Heroicon;
 use Filament\Tables\Table;
+use App\Filament\Resources\Categories\RelationManagers\PostsRelationManager;
 
 class CategoryResource extends Resource
 {
@@ -35,7 +36,7 @@ class CategoryResource extends Resource
     public static function getRelations(): array
     {
         return [
-            \App\Filament\Resources\Categories\RelationManagers\PostsRelationManager::class,
+            PostsRelationManager::class,
         ];
     }
 
--- app/Filament/Resources/Categories/RelationManagers/PostsRelationManager.php
+++ app/Filament/Resources/Categories/RelationManagers/PostsRelationManager.php
@@ -2,21 +2,55 @@
 
 namespace App\Filament\Resources\Categories\RelationManagers;
 
+use Filament\Forms\Components\TextInput;
 use Filament\Resources\RelationManagers\RelationManager;
 use Filament\Tables;
 use Filament\Tables\Table;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Schemas\Schema;
+use Filament\Actions\AssociateAction;
 
 class PostsRelationManager extends RelationManager
 {
     protected static string $relationship = 'posts';
 
+    public function form(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('title')
+                    ->required()
+                    ->maxLength(255),
+
+                TextInput::make('slug')
+                    ->required()
+                    ->maxLength(255),
+            ]);
+    }
+
     public function table(Table $table): Table
     {
         return $table
+            ->recordTitleAttribute('title')
             ->columns([
-                Tables\Columns\TextColumn::make('title')
-                    ->searchable()
+                TextColumn::make('title')
+                    ->searchable(),
+
+                TextColumn::make('slug')
+                    ->searchable(),
+
+                TextColumn::make('created_at')
+                    ->dateTime()
                     ->sortable(),
+            ])
+            ->headerActions([
+                \Filament\Actions\CreateAction::make(),
+                \Filament\Actions\AssociateAction::make(),
+            ])
+
+            ->actions([
+                \Filament\Actions\EditAction::make(),
+                \Filament\Actions\DeleteAction::make(),
             ]);
     }
 }
